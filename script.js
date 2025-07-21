82387af13003da0566bf5834b41bd165e08aec5f
MarcosAntonio77 <santomarcos059@gmail.com>
2025-07-21 10:55:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// Armazena o índice atual de cada carrossel por classe
+  const slideIndices = {
+    mySlides1: 1,
+    mySlides01: 1
+  };
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  function showSlides(slideClass) {
+    const slides = document.getElementsByClassName(slideClass);
+    let index = slideIndices[slideClass];
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    if (index > slides.length) slideIndices[slideClass] = 1;
+    if (index < 1) slideIndices[slideClass] = slides.length;
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[slideIndices[slideClass] - 1].style.display = "block";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+  function changeSlide(slideClass, n) {
+    slideIndices[slideClass] += n;
+    showSlides(slideClass);
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+
+  // Inicializa ambos os slides mostrando o primeiro
+  showSlides('mySlides1');
+  showSlides('mySlides01');
